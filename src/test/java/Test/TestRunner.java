b4f6794e91a3584a1a74b66c6417b3218c913bf1
pravinkumbhare <pravin.kumbhare1@gmail.com>
2023-02-25 00:59:04 +0530

    cucumber report generated

--- src/test/java/Test/TestRunner.java
+++ src/test/java/Test/TestRunner.java
@@ -2,7 +2,15 @@ package Test;
 
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 
 //@RunWith(Karate.class)
@@ -13,7 +21,19 @@ public class TestRunner
 	@Test
     public void testParallel() {
         Results results = Runner.path("classpath:Test")
+				.outputCucumberJson(true)
         		.tags("@Pallavi")
         		.parallel(1);
-}
+				generateReport(results.getReportDir());
+		}
+
+	public static void generateReport(String karateOutputPath) {
+
+		Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
+		List<String> jsonPaths = new ArrayList<String>(jsonFiles.size());
+		jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+		Configuration config = new Configuration(new File("target"), "KarateCucumberReport");
+		ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+		reportBuilder.generateReports();
+	}
 }
